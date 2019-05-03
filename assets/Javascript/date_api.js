d4ba7975c57cc5ac0ab732f705084bdc82972ab5
Erica Walsh <erica.f.walsh@gmail.com>
2019-05-02 21:34:50 -0400

    updates

--- assets/Javascript/date_api.js
+++ assets/Javascript/date_api.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
 
     var holidayName;
     var occasion_date;
-    
 
-    $('.holidays').on('change',function(){
+
+    $('.holidays').on('change', function () {
 
         holidayName = $(".holidays option:selected").val();
 
@@ -16,16 +16,16 @@ $(document).ready(function () {
 
             $("#customDateForm").show();
 
-            $('#customDateForm').on('change',function(){
+            $('#customDateForm').on('change', function () {
 
                 occasion_date = new Date($("#userProvidedDate").val());
-        
+
             });
-            
-        } 
+
+        }
     });
 
-    
+
     $("#search-button").on("click", calendar);
 
 
@@ -35,46 +35,46 @@ $(document).ready(function () {
 
             var today = new Date();
             var days_until = Math.round((occasion_date - today) / (1000 * 60 * 60 * 24));
-    
+
             $("#daysRemaining").html("You have " + days_until + " days left to buy your book!");
         }
 
         else {
 
-        var apiKey = "c65ffe3f62969801df573185b16ae4961aa65370";
-        var queryURL = "https://calendarific.com/api/v2/holidays?country=US&year=2019&api_key=" + apiKey;
+            var apiKey = "c65ffe3f62969801df573185b16ae4961aa65370";
+            var queryURL = "https://calendarific.com/api/v2/holidays?country=US&year=2019&api_key=" + apiKey;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
 
-                var results = response.response.holidays;
+                    var results = response.response.holidays;
 
-                for (var i = 0; i < results.length; i++) {
+                    for (var i = 0; i < results.length; i++) {
 
-                    if (results[i].name === holidayName) {
+                        if (results[i].name === holidayName) {
 
-                        var date = results[i].date.iso;
+                            var date = results[i].date.iso;
 
-                        var today = new Date();
-                        var occasion_date = new Date(date);
+                            var today = new Date();
+                            var occasion_date = new Date(date);
 
-                        var days_until = Math.round((occasion_date - today) / (1000 * 60 * 60 * 24));
+                            var days_until = Math.round((occasion_date - today) / (1000 * 60 * 60 * 24));
 
-                        if (days_until < 0) {
-                            days_until = days_until + 365;
+                            if (days_until < 0) {
+                                days_until = days_until + 365;
+                            }
+
+                            $("#daysRemaining").html("You have " + days_until + " days left to buy your book!");
                         }
 
-                        $("#daysRemaining").html("You have " + days_until + " days left to buy your book!");
-                    }
+                    };
 
-                };
+                });
 
-            });
+        };
 
     };
-
-};
 });
